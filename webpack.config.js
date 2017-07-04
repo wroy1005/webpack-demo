b4aaf0c114e2ab8cdb244fa2acb72784e2f376b1
wroy1005 <wroy1005@163.com>
2017-07-04 16:32:58 +0800

    add some plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        app: "./src/index.js"
+    },
+
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
-    }
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+    },
+
+    devtool: "source-map",
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            // title: The title to use for the generated HTML document.
+            title: "HTML demo",
+            //  The file to write the HTML to, base on output.path
+            filename: "admin.html",
+            // template:  Webpack require path to the template.
+            template: "src/index.html",
+            // Pass html-minifier's options as object to minify the output.
+            minify: {
+                // Collapse white space that contributes to text nodes in a document tree
+                collapseWhitespace: false
+            },
+            // if true then append a unique webpack compilation hash to all included scripts and CSS files
+            hash: true,
+            // Allows you to add only some chunks (e.g. only the unit-test chunk)
+            chunks: ['app']
+        })
+    ]
 }
