bdc1e21a78748c818dd02a501bd7d4875c59c883
wroy1005 <wroy1005@163.com>
2017-07-04 14:52:49 +0800

    init for output management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from "lodash";
-import './style.css';
-import Icon from "./icon.png";
-import Data from "./data.xml";
 
 function component() {
     var element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add("hello");
-
-    // add image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
 
     return element;
 }
