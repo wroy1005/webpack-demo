cd8f847b5b112c441569368912c7bb253a551a9d
author <author@corpmail.com>
2017-09-05 13:52:06 +0800

    添加html-webpack-plugin插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 var path = require('path');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports= {
     entry: "./src/index.js",
     output: {
         path: path.resolve('dist'),
         filename: "bundle.js"
-    }
+    },
+    plugins: [new HtmlWebpackPlugin()]
 }
