5f3402ae81b8e62d4743a7901d0c5e260e208daa
author <author@corpmail.com>
2017-09-05 14:36:49 +0800

    配置scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+var css = require('./scss/app.scss'); // 使用相对路径
+
 console.log('123');
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,22 @@ module.exports = {
         path: path.resolve('dist'),
         filename: "bundle.js"
     },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'My App', 
-            template: './src/index.html', 
+            title: 'My App',
+            template: './src/index.html',
             minify: {
                 collapseWhitespace: true
             },
