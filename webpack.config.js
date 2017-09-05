7277fc0245fb6bed2a7dc7aeafc1ebbba7640e45
author <author@corpmail.com>
2017-09-05 14:01:28 +0800

    更多选项配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports= {
+module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve('dist'),
         filename: "bundle.js"
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App', 
+            template: './src/index.html', 
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            filename: 'output.html' // base on dist
+        })
+    ]
 }
